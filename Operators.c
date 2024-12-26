9c08a59caae850411af0ab5b14117de5b01bf380
Dharunkumar Venkateshwaran <bhuvandharun0419@gmail.com>
2024-12-26 20:52:57 +0530

    Create Operators.c

---
+++ Operators.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d\n",&a);
+    scanf("%d",&b);
+    int add=a+b;
+    printf("%d\n",add);
+    int sub=a-b;
+    printf("%d\n",sub);
+    int mul=a*b;
+    printf("%d\n",mul);
+    int mod=a%b;
+    printf("%d\n",mod);
+    int divi=a/b;
+    printf("%d",divi);
+    
+    return 0;
+}
